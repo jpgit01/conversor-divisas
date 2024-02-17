e6e121a74dc930679ebec69203d67f0b52c52911
Juan Pablo M. <pablochileidh@gmail.com>
2024-02-17 12:43:29 -0300

    solucion problema tipo moneda

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,26 +4,26 @@ const mostrarResult = document.getElementById("result");
 const mostrarGraf = document.getElementById("grafico");
 const btnBuscar = document.getElementById("btn_buscar");
 
-
-
-btnBuscar.addEventListener("click", myFunction);
-
-function myFunction() {
-  document.getElementById("result").innerHTML = pesoChileno.value;
+async function divisaFetch(ele) {
+  try {
+    const res = await fetch(`https://mindicador.cl/api/${ele}/`);
+    const data = await res.json();
+    const valorMoneda = data.serie[0].valor;
+    console.log(valorMoneda)
+    let calculo = pesoChileno.value / valorMoneda
+    return calculo.toFixed(2);
+  } catch (e) {
+    mostrarResult.innerHTML = `Error Resultado: ${e.message}`;
+  }
 }
 
+btnBuscar.addEventListener("click", insertarResult);
+async function insertarResult() {
+    try{
+        let tipoCambio = monedaConvertir.value
+        mostrarResult.innerHTML= await divisaFetch(tipoCambio)
+    }catch (e) {
+        mostrarResult.innerHTML = `Error Resultado: ${e.message}`;
+      }
 
-async function divisaFetch() {
-    try {
-    const res = await fetch("https://mindicador.cl/api/dola/");
-    const data = await res.json();
-    console.log(data.serie[0].valor);
-    } catch (e) {
-    mostrarResult.innerHTML = `Error Resultado ${e.message}`;
-    
-    }
-
-
-
-    }
-    divisaFetch();
+}
